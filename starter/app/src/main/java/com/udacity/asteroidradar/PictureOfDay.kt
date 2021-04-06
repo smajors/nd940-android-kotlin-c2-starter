135b1e9c3bd6182579c1aab62ebfc98fe6217b1c
Stephen Majors <stephen@majorscreations.com>
2021-04-06 15:38:05 -0500

    Fix for incorrect JSON mapping
    - PictureOfDay.kt
    -- Reordered parameters to match JSON object being returned.

--- starter/app/src/main/java/com/udacity/asteroidradar/PictureOfDay.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/PictureOfDay.kt
@@ -2,5 +2,5 @@ package com.udacity.asteroidradar
 
 import com.squareup.moshi.Json
 
-data class PictureOfDay(@Json(name = "media_type") val date: String, val mediaType: String, val title: String,
+data class PictureOfDay(val date: String, @Json(name = "media_type") val mediaType: String, val title: String,
                         val url: String)
